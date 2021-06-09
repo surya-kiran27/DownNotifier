8c8cb112eb7960a363ed4ff6726e5676f937bc03
surya-kiran27 <suryaraju67@gmail.com>
2021-06-09 19:47:04 +0530

    remove dot env config

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var cors = require("cors");
 var logger = require("morgan");
-require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
