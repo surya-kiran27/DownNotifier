6ee00ca64e4c93af17d402de593e54ffe51b3a21
surya-kiran27 <suryaraju67@gmail.com>
2021-06-09 19:30:26 +0530

    add local config file

--- environments/local.env.js
+++ environments/local.env.js
@@ -2,10 +2,10 @@ module.exports = {
   databases: {
     downNotifier: {
       dbName: "downNotifier",
-      host: "143.110.179.210",
+      host: "localhost",
       port: "27017",
-      username: process.env.dbUsername,
-      password: process.env.dbPassword,
+      username: "admin",
+      password: "abc123",
     },
   },
 };
