c6e6d56125d3d7c485149c55e765576369fc5eb5
surya-kiran27 <suryaraju67@gmail.com>
2021-06-09 19:26:33 +0530

    add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "down-notifier-dev",
+      script: 'npm',
+      args: 'start',
+      watch: 'false',
+      env: {
+        NODE_ENV: "dev",
+        domain = "https://143.110.179.210/down-notifier"
+      },
+    },
+  ],
+};
