156f207d8564a7fc6469278a53c8106b4d1b88df
surya-kiran27 <suryaraju67@gmail.com>
2021-06-16 19:34:52 +0530

    add try catch for scraper

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -12,46 +12,54 @@ async function checkState() {
       "--disable-dev-shm-usage",
       "--disable-setuid-sandbox",
       "--no-sandbox",
-      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process", // <- this one doesn't works in Windows
+      "--disable-gpu",
     ],
   });
-  let page = await browser.newPage();
-  await page.goto("https://downdetector.com/status/instagram/archive/");
+  try {
+    let page = await browser.newPage();
+    await page.goto("https://downdetector.com/status/instagram/archive/");
+    await (await page.waitForSelector("#_evidon-banner-acceptbutton")).click();
 
-  await (await page.waitForSelector("#_evidon-banner-acceptbutton")).click();
-
-  const data = await page.evaluate(() => {
-    const tds = Array.from(document.querySelectorAll("table tr td a"));
-    return tds.map((td) => {
-      return { company: `${td.innerText}`, link: td.getAttribute("href") };
+    const data = await page.evaluate(() => {
+      const tds = Array.from(document.querySelectorAll("table tr td a"));
+      return tds.map((td) => {
+        return { company: `${td.innerText}`, link: td.getAttribute("href") };
+      });
     });
-  });
-  let instagram = data.find((obj) => obj.company === "Instagram");
-  console.log("instagram", instagram);
-  if (instagram) {
-    await page.goto(`https://downdetector.com/${instagram.link}`);
-    await page.waitForSelector(".chartjs-render-monitor");
-    const dataUrlChart = await page.evaluate(() => {
-      const canvas = document.querySelector(".chartjs-render-monitor");
-      return canvas.toDataURL();
-    });
+    let instagram = data.find((obj) => obj.company === "Instagram");
+    console.log("instagram", instagram);
+    if (instagram) {
+      await page.goto(`https://downdetector.com/${instagram.link}`);
+      await page.waitForSelector(".chartjs-render-monitor");
+      const dataUrlChart = await page.evaluate(() => {
+        const canvas = document.querySelector(".chartjs-render-monitor");
+        return canvas.toDataURL();
+      });
 
-    let base64String = dataUrlChart.substr(dataUrlChart.indexOf(",") + 1); // get everything after the comma
-    let imgBuffer = Buffer.from(base64String, "base64"); //
-    let published = await page.$eval(".date", (el) => el.innerText);
-    published = published.split(": ")[1].trim();
-    published = published.slice(0, -1);
-    //text-justify
-    let info = await page.$eval(".text-justify", (el) => el.innerText);
-    info = info.split(".")[0];
-    const eventDoc = await Event.findOne({ originTime: published });
-    console.log("published", published, "eventDoc", eventDoc);
-    if (!eventDoc) {
-      await Event.create({ status: info, originTime: published });
-      await sendBulk({ info, published, image: imgBuffer });
+      let base64String = dataUrlChart.substr(dataUrlChart.indexOf(",") + 1); // get everything after the comma
+      let imgBuffer = Buffer.from(base64String, "base64"); //
+      let published = await page.$eval(".date", (el) => el.innerText);
+      published = published.split(": ")[1].trim();
+      published = published.slice(0, -1);
+      //text-justify
+      let info = await page.$eval(".text-justify", (el) => el.innerText);
+      info = info.split(".")[0];
+      const eventDoc = await Event.findOne({ originTime: published });
+      console.log("published", published, "eventDoc", eventDoc);
+      if (!eventDoc) {
+        await Event.create({ status: info, originTime: published });
+        await sendBulk({ info, published, image: imgBuffer });
+      }
     }
+    if (browser != null) await browser.close();
+  } catch (error) {
+    if (browser != null) await browser.close();
+    console.log("error", error);
   }
-  if (browser != null) await browser.close();
 }
 
 const job = schedule.scheduleJob(
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "npm",
       args: "start",
       watch: "false",
+      max_memory_restart: "200M",
       env: {
         NODE_ENV: "dev",
         domain: "https://143.110.179.210/down-notifier",
